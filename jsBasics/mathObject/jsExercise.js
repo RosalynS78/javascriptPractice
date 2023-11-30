df62a1ab04d38d6979271a051c53ec06ba45f7d9
Rosalyn Scheffel <49538043+RosalynS78@users.noreply.github.com>
2023-11-29 19:51:55 -0600

    more practices

---
+++ jsBasics/mathObject/jsExercise.js
@@ -0,0 +1,12 @@
+// write integer between 1 - 10 
+//user is prompted to input "guess Number"
+//displays "Good Work" matches
+//otherwise not matched
+
+
+//Math.floor()
+//Math.round()
+
+function random
+	const guessNum())
+		guessNum.Math.random(Math)
