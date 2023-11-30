b5ec5046bcadb414c4414c5b4b48b6d94ab28a83
Rosalyn Scheffel <49538043+RosalynS78@users.noreply.github.com>
2023-11-29 20:03:06 -0600

    understand

--- jsBasics/mathObject/jsExercise.js
+++ jsBasics/mathObject/jsExercise.js
@@ -30,4 +30,43 @@ if (gnum == num)
 else
     // Log a message if the guessed number does not match, and also provide the correct number
     console.log('Not matched, the number was ' + gnum); 
+    
+    
+    
+let's generate random numbers between 1 and 10
+
+ex.
+
+const step1 = Math.random( );
+
+//0.5961104892810127
+
+const step2 = step1 * 10
+
+//5.961104892810127
+
+const step3 = Math.floor(step2);
+
+//5
+
+const step4 = step3 + 1;
+
+//6
+
+Math.floor(Math.random( ) * 10) + 1;
+
+ex. 1-5
+
+Math.floor(Math.random( ) * 5); // returns random 0-4
+
+need to shift everything up by adding one
+
+Math.floor(Math.random( ) * 5) + 1 // returns random 1-5
+
+ex. from 22
+
+Math.floor(Math.random( ) * 3 + 20);
+
+(20, 21, and 22) that's 3 numbers then shift this upwars by 20
+
    
